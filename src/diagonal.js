749bdc977de350456e33eafcc653702700b2b9c1
Paniz Ahang <pahang@ucvts.org>
2019-10-24 09:48:08 -0400

    diagonal

--- src/diagonal.js
+++ src/diagonal.js
@@ -2,4 +2,6 @@ const readlineSync = require("readline-sync");
 
 const width = readlineSync.question("Width: ");
 const length = readlineSync.question("Length: ");
-const diagonal
+const root = Math.hypot(width,length).toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+console.log("A(n) "+ width +"-by-"+ length +"-inch sheet of paper has a diagnal of "+ root +" inch(es).");
